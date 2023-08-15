324ecbc1fae99eb6a6f1210ade2f5366a7bc8670
onesixx <onesixx@nate.com>
2023-08-15 23:42:36 +0900

    j

--- snake_game.py
+++ snake_game.py
@@ -1,14 +1,16 @@
 # https://github.com/TheAILearner/Snake-Game-using-OpenCV-Python/blob/master/snake_game_using_opencv.ipynb
 
 import numpy as np
-import cv2
+import cv2           #opencv
 import random
 import time
 
 def collision_with_apple(apple_position, score):
     score += 1
-    apple_position = [random.randrange(1,50)*10,
-                      random.randrange(1,50)*10]
+    apple_position = [
+        random.randrange(1,50)*10,
+        random.randrange(1,50)*10
+    ]
     return apple_position, score
 
 def collision_with_boundaries(snake_head):
@@ -25,6 +27,9 @@ def collision_with_self(snake_position):
         return 0
 
 img = np.zeros((500,500, 3),dtype='uint8')
+# cv2.imshow('Image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 # Initial Snake and Apple position
 snake_position = [[250,250],[240,250],[230,250]]
@@ -40,7 +45,11 @@ while True:
     img = np.zeros((500,500,3), dtype='uint8')
 
     # Display Apple
-    cv2.rectangle(img,(apple_position[0], apple_position[1]), (apple_position[0]+10,apple_position[1]+10), (0,0,255), 3)
+    cv2.rectangle(img,
+        (apple_position[0], apple_position[1]),
+        (apple_position[0]+10,apple_position[1]+10),
+        (0,0,255),
+        3)
     # Display Snake
     for position in snake_position:
         cv2.rectangle(img,(position[0],position[1]), (position[0]+10,position[1]+10),(0,255,0),3)
